0b55c6200cdd4ca3eacf4cf236ea31ec69356c0d
ahmedWa1408 <aa1229550@gmail.com>
2026-06-23 08:07:35 +0300

    Implement supervisor management functions
    Added functions to manage supervisors in local storage.

--- data/supervisors.js
+++ data/supervisors.js
@@ -1,1 +1,48 @@
+function getSupervisors() {
+    return JSON.parse(localStorage.getItem("supervisors")) || [];
+}
 
+function saveSupervisor(supervisor) {
+
+    let list = getSupervisors();
+
+    const index = list.findIndex(item =>
+        item.id === supervisor.id
+    );
+
+    if (index >= 0) {
+
+        list[index] = supervisor;
+
+    } else {
+
+        list.push(supervisor);
+
+    }
+
+    localStorage.setItem("supervisors", JSON.stringify(list));
+
+}
+
+function deleteSupervisor(id) {
+
+    let list = getSupervisors();
+
+    list = list.filter(item =>
+        item.id !== id
+    );
+
+    localStorage.setItem("supervisors", JSON.stringify(list));
+
+}
+
+function searchSupervisor(keyword) {
+
+    const list = getSupervisors();
+
+    return list.filter(item =>
+        item.name.includes(keyword) ||
+        item.id.includes(keyword)
+    );
+
+}
