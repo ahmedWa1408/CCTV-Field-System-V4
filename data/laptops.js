e6c279dfaeb46e2273b418c458d31a715e8db134
ahmedWa1408 <aa1229550@gmail.com>
2026-06-23 08:06:13 +0300

    Add functions to manage laptops in localStorage

--- data/laptops.js
+++ data/laptops.js
@@ -1,1 +1,47 @@
+function getLaptops() {
+    return JSON.parse(localStorage.getItem("laptops")) || [];
+}
 
+function saveLaptop(laptop) {
+
+    let list = getLaptops();
+
+    const index = list.findIndex(item =>
+        item.number === laptop.number
+    );
+
+    if (index >= 0) {
+
+        list[index] = laptop;
+
+    } else {
+
+        list.push(laptop);
+
+    }
+
+    localStorage.setItem("laptops", JSON.stringify(list));
+
+}
+
+function deleteLaptop(number) {
+
+    let list = getLaptops();
+
+    list = list.filter(item =>
+        item.number !== number
+    );
+
+    localStorage.setItem("laptops", JSON.stringify(list));
+
+}
+
+function searchLaptop(keyword) {
+
+    const list = getLaptops();
+
+    return list.filter(item =>
+        item.number.includes(keyword)
+    );
+
+}
