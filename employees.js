f9fd327ab044838b0d3c1a5d04162a451fab2762
ahmedWa1408 <aa1229550@gmail.com>
2026-06-23 08:01:53 +0300

    Implement employee management functions
    Added functions to manage employee data in local storage.

--- employees.js
+++ employees.js
@@ -1,1 +1,46 @@
+const employees = [];
 
+function getEmployees() {
+    return JSON.parse(localStorage.getItem("employees")) || [];
+}
+
+function saveEmployee(employee) {
+
+    let list = getEmployees();
+
+    const index = list.findIndex(item => item.id === employee.id);
+
+    if (index >= 0) {
+
+        list[index] = employee;
+
+    } else {
+
+        list.push(employee);
+
+    }
+
+    localStorage.setItem("employees", JSON.stringify(list));
+
+}
+
+function deleteEmployee(id) {
+
+    let list = getEmployees();
+
+    list = list.filter(item => item.id !== id);
+
+    localStorage.setItem("employees", JSON.stringify(list));
+
+}
+
+function searchEmployee(keyword) {
+
+    const list = getEmployees();
+
+    return list.filter(item =>
+        item.name.includes(keyword) ||
+        item.id.includes(keyword)
+    );
+
+}
